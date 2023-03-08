d8056646dd30b95981d8011fa5dea49f4106802e
Sheryl Boles <sheryl.boles@gmail.com>
2023-03-07 22:27:28 -0500

    initial commit

---
+++ components/shared/navbar.js
@@ -0,0 +1,24 @@
+import renderToDOM from '../../utils/renderToDom';
+
+const navBar = () => {
+  const domString = `
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">tekCards</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Create New Link</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>`;
+
+  renderToDOM('navigation', domString);
+};
+
+export default navBar;
